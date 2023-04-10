2f0021e247c231c9abbe07153020ad14b8ce9e30
sarcan <sarmayca@protonmail.ch>
2023-04-10 10:08:58 +0200

    Added ripple effect, modal & image, changed style

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
+import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { Button, FlatList, StyleSheet, View } from "react-native";
 
 import GoalInput from "./components/GoalInput";
 import GoalItem from "./components/GoalItem";
 
 export default function App() {
+    const [modalIsVisible, setModalIsVisible] = useState(false);
     const [courseGoals, setCourseGoals] = useState([]);
+
+    function startAddGoalHandler() {
+        setModalIsVisible(true);
+    }
+
+    function endAddGoalHandler() {
+        setModalIsVisible(false);
+    }
 
     function addGoalHandler(enteredGoalText) {
         setCourseGoals((currentCourseGoals) => [
             ...currentCourseGoals,
             { text: enteredGoalText, id: Math.random().toString() },
         ]);
+        endAddGoalHandler();
     }
 
     function deleteGoalHandler(id) {
@@ -21,27 +32,41 @@ export default function App() {
     }
 
     return (
-        <View style={styles.appContainer}>
-            <GoalInput onAddGoal={addGoalHandler} />
-            <View style={styles.goalsContainer}>
-                <FlatList
-                    data={courseGoals}
-                    renderItem={(itemData) => {
-                        return (
-                            <GoalItem
-                                text={itemData.item.text}
-                                id={itemData.item.id}
-                                onDeleteItem={deleteGoalHandler}
-                            />
-                        );
-                    }}
-                    keyExtractor={(item, index) => {
-                        return item.id;
-                    }}
-                    alwaysBounceVertical={false}
+        <>
+            <StatusBar style="light" />
+            <View style={styles.appContainer}>
+                <Button
+                    title="Add New Goal"
+                    color="#a065ec"
+                    onPress={startAddGoalHandler}
                 />
+                {modalIsVisible && (
+                    <GoalInput
+                        visible={modalIsVisible}
+                        onAddGoal={addGoalHandler}
+                        onCancel={endAddGoalHandler}
+                    />
+                )}
+                <View style={styles.goalsContainer}>
+                    <FlatList
+                        data={courseGoals}
+                        renderItem={(itemData) => {
+                            return (
+                                <GoalItem
+                                    text={itemData.item.text}
+                                    id={itemData.item.id}
+                                    onDeleteItem={deleteGoalHandler}
+                                />
+                            );
+                        }}
+                        keyExtractor={(item, index) => {
+                            return item.id;
+                        }}
+                        alwaysBounceVertical={false}
+                    />
+                </View>
             </View>
-        </View>
+        </>
     );
 }
 
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { Button, StyleSheet, TextInput, View } from "react-native";
+import {
+    Button,
+    Image,
+    Modal,
+    StyleSheet,
+    TextInput,
+    View,
+} from "react-native";
 
 function GoalInput(props) {
     const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -14,15 +21,36 @@ function GoalInput(props) {
     }
 
     return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                style={styles.textInput}
-                placeholder="Your course goal!"
-                onChangeText={goalInputHandler}
-                value={enteredGoalText}
-            />
-            <Button title="Add Goal" onPress={addGoalHandler} />
-        </View>
+        <Modal visible={props.visible} animationType="slide">
+            <View style={styles.inputContainer}>
+                <Image
+                    style={styles.image}
+                    source={require("../assets/images/goal.png")}
+                />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Your course goal!"
+                    onChangeText={goalInputHandler}
+                    value={enteredGoalText}
+                />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button
+                            title="Cancel"
+                            onPress={props.onCancel}
+                            color="#f31282"
+                        />
+                    </View>
+                    <View style={styles.button}>
+                        <Button
+                            title="Add Goal"
+                            onPress={addGoalHandler}
+                            color="#b180f0"
+                        />
+                    </View>
+                </View>
+            </View>
+        </Modal>
     );
 }
 
@@ -31,18 +59,31 @@ export default GoalInput;
 const styles = StyleSheet.create({
     inputContainer: {
         flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
+        justifyContent: "center",
         alignItems: "center",
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: "#cccccc",
+        padding: 16,
+        backgroundColor: "#311b6b",
+    },
+    image: {
+        width: 100,
+        height: 100,
+        margin: 20,
     },
     textInput: {
         borderWidth: 1,
-        borderColor: "#cccccc",
-        width: "70%",
-        marginRight: 8,
-        padding: 8,
+        borderColor: "#e4d0ff",
+        backgroundColor: "#e4d0ff",
+        color: "#120438",
+        borderRadius: 6,
+        width: "100%",
+        padding: 16,
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        marginTop: 16,
+    },
+    button: {
+        width: 100,
+        marginHorizontal: 8,
     },
 });
